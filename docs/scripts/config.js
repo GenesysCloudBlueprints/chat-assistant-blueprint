1a163124f0b2d515e2b6fc3e0bc543545e08e98d
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2022-10-03 08:23:26 +0800

    Add ID

--- docs/scripts/config.js
+++ docs/scripts/config.js
@@ -1,5 +1,5 @@
 export default {
-    clientID: 'your-client-id-here',
+    clientID: '5f3e661d-61be-4a13-b536-3f54f24e26c9',
 
     testUri: 'https://localhost/',
     prodUri:  'https://genesyscloudblueprints.github.io/chat-assistant-blueprint/',
